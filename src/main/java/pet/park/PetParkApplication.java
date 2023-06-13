3657e4ebeee432fd3c2a566fb2b499de429d335e
JoleneMel <jolenemelanson33@gmail.com>
2023-06-13 13:11:11 -0400

    Week 13

--- src/main/java/pet/park/PetParkApplication.java
+++ src/main/java/pet/park/PetParkApplication.java
@@ -3,10 +3,17 @@ package pet.park;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Very important for all SpringBoot apps 
+//This enables component scan and auto-configuration
+//Component scan loads everything, manages beans etc.
+//Conponent scan also is great within our controller class, it will map our HTTP requests to methods that we write
+//We use annotations to communicate to Spring which HTTP verb, which can be mapped via java methods
+//Auto Configuration 
 @SpringBootApplication
 public class PetParkApplication {
 
 	public static void main(String[] args) {
+		//This is the run class where we pass on our class within it
 		SpringApplication.run(PetParkApplication.class, args);
 	}
 
