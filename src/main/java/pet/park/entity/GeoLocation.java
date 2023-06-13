7f6d37940dd5ad43c86b26412f5e63eec31f2959
JoleneMel <jolenemelanson33@gmail.com>
2023-06-13 18:56:24 -0400

    geolocation notes added

--- src/main/java/pet/park/entity/GeoLocation.java
+++ src/main/java/pet/park/entity/GeoLocation.java
@@ -7,13 +7,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 //shortcut for import is ctrl + space + enter
 
+
 @Embeddable
+//Creates getters and setters, hashcode and equals methods. 
 @Data
+//Creates constructor 
 @NoArgsConstructor
 public class GeoLocation {
+	//Like long with a set number of decimal points)
 	private BigDecimal latitude;
 	private BigDecimal longitude;
 	
+	//This is a copy constructor, used when taking objects
 	public GeoLocation(GeoLocation geoLocation) {
 		this.latitude = geoLocation.latitude;
 		this.longitude = geoLocation.longitude;
