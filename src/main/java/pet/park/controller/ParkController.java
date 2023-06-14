86a1e79deda59a1526548c20cd240a2c688af7c9
JoleneMel <jolenemelanson33@gmail.com>
2023-06-13 20:01:58 -0400

    notes added

--- src/main/java/pet/park/controller/ParkController.java
+++ src/main/java/pet/park/controller/ParkController.java
@@ -3,7 +3,7 @@ package pet.park.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
+//import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
